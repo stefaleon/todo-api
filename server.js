8c067fe5b455cc6fd1ef1fb1deb397ef32319aeb
maimous@gmail.com <stefaleon@gmail.com>
2016-10-26 00:28:41 +0300

    refactoring and validation using _.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -20,22 +21,39 @@ app.get('/todos', function(req, res){
 });
 
 // GET/todos/:id
-app.get('/todos/:someId', function(req, res){	
-	todos.forEach(function(todo){
-		if (todo.id === parseInt(req.params.someId)){
-			res.json(todo);
-		}
-	});
-	res.status(404).send();
+app.get('/todos/:someId', function(req, res){		
+	// searching for a todo with a matching id in the todos array 
+	// by use of the _.findWhere method
+	// the id has to match the request parameters entered -> someId
+	var matchedTodo = _.findWhere(todos, { id: parseInt(req.params.someId) });
+	if (matchedTodo){
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}	
 });
 
 // POST /todos
 app.post('/todos', function(req, res){
-	var body = req.body; 
+	// use the _.pick method to maintain only the
+	// 'description' and 'completed' key-value pairs
+	// even if more keys and values are posted with the request object
+
+	var body = _.pick(req.body, 'description', 'completed'); 
 	// use the postman app to post a json object such as
 	// {"description": "buy milk",	"completed": false}
-	body.id = todoNextId;
-	todoNextId++;
+	if ( !_.isString(body.description) || !_.isBoolean(body.completed)
+		|| body.description.trim().length === 0 )
+		return res.status(400).send();
+
+
+	// add id field and increase the todoNextId var by one
+	body.id = todoNextId++;
+
+	// clear description from blanks
+	body.description = body.description.trim();
+
+	// push body to the todos array
 	todos.push(body);
 
 
