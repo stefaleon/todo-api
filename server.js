988e6666a5a6769b94404f4dc65b6bad007b0680
maimous@gmail.com <stefaleon@gmail.com>
2016-10-26 02:45:23 +0300

    updating todos by adding the PUT route

--- server.js
+++ server.js
@@ -35,13 +35,16 @@ app.get('/todos/:someId', function(req, res){
 
 // POST /todos
 app.post('/todos', function(req, res){
+	
 	// use the _.pick method to maintain only the
 	// 'description' and 'completed' key-value pairs
 	// even if more keys and values are posted with the request object
-
 	var body = _.pick(req.body, 'description', 'completed'); 
+
 	// use the postman app to post a json object such as
 	// {"description": "buy milk",	"completed": false}
+	// validate that 'description' is a non empty string 
+	// and that 'completed' is a string
 	if ( !_.isString(body.description) || !_.isBoolean(body.completed)
 		|| body.description.trim().length === 0 )
 		return res.status(400).send();
@@ -68,14 +71,56 @@ app.delete('/todos/:someId', function(req, res){
 	// the id has to match the request parameters entered -> someId
 	var matchedTodo = _.findWhere(todos, { id: parseInt(req.params.someId) });
 	if (matchedTodo){
+		// remove the matched todo from the todos array
 		todos = _.without(todos, matchedTodo);
 		res.json(matchedTodo);
-		//res.status(200).send();
+		//responding with the deleted todo instead of res.status(200).send();
 	} else {
 		res.status(404).send().json({"error" : "no todo found with that id"});
 	}	
 });
 
+// PUT/todos/:id
+app.put('/todos/:someId', function(req, res){
+
+	// use the _.pick method to maintain only the
+	// 'description' and 'completed' key-value pairs
+	// even if more keys and values are being send with the request object
+	var body = _.pick(req.body, 'description', 'completed'); 
+	var validAttributes = {};
+
+	// check if 'completed' exists and validates
+	// then add to validAttributes object
+	if ( body.hasOwnProperty('completed') && _.isBoolean(body.completed) ){
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		res.status(404).send()
+	} 
+
+	// check if 'description' exists and validates
+	// then add to validAttributes object
+	if ( body.hasOwnProperty('description') &&
+			   _.isString(body.description) &&
+			   body.description.trim().length > 0 ){
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		res.status(404).send()
+	} 
+
+
+	// searching for a todo with a matching id in the todos array 
+	// by use of the _.findWhere method
+	// the id has to match the request parameters entered -> someId
+	var matchedTodo = _.findWhere(todos, { id: parseInt(req.params.someId) });
+	if (matchedTodo){
+		// update the matched todo
+		_.extend(matchedTodo, validAttributes);
+		res.json(matchedTodo);
+		//responding with the matched todo instead of res.status(200).send();
+	} else {
+		res.status(404).send().json({"error" : "no todo found with that id"});
+	}	
+});
 
 app.listen(PORT, function(){
 	console.log('Express server started on port ' + PORT);
